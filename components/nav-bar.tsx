deeee27d9e22198b6e6257c9f64f82361458ce74
Nuwan Perera <nuwanperera613@gmail.com>
2024-04-28 22:47:09 +0530

    chore: add gradient effect to home button

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -11,9 +11,9 @@ export default function NavBar(props: NavBarProps) {
     <nav className={cn("w-full p-4 fixed", className)}>
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <Link href="/">
-          <p className="text-2xl font-semibold text-zinc-50">Aughh</p>
+          <p className="font-semibold bg-clip-text text-transparent text-2xl bg-gradient-to-t from-zinc-700 to-zinc-400">Aughh</p>
         </Link>
-        <Link href="/subscribe" className="inline-flex justify-center items-center py-2 px-4 border bg-zinc-950 border-zinc-800 transition-all duration-300 hover:bg-zinc-800 rounded-full font-medium text-sm text-zinc-200">
+        <Link href="/subscribe" className="py-2 px-4 border bg-zinc-950 border-zinc-800 transition-all duration-300 hover:bg-zinc-800 rounded-full font-medium text-sm text-zinc-200">
           Subscribe
         </Link>
       </div>
