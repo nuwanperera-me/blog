740be2f0774f178b58706443303655b0243e4121
Nuwan Perera <nuwanperera613@gmail.com>
2024-04-29 19:29:43 +0530

    fix: add utf-8 emoji

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
         <h1 className="bg-clip-text bg-gradient-to-t from-zinc-700 to-zinc-400 text-transparent text-4xl font-semibold">
           Plyn htt ynn!
         </h1>
-        <span className="pl-2 text-4xl">😾</span>
+        <span className="pl-2 text-4xl">&#128574;</span>
       </div>
       <p className="text-lg max-w-96 text-center text-zinc-600 pt-4">
         The page you're looking for doesn't exist. Please check the URL or click
