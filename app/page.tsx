ef01abd5223313d3f7e3c6026d5cc4f169e2f0a8
Nuwan Perera <nuwanperera613@gmail.com>
2024-04-29 18:50:27 +0530

    chore: remove unnecessary codes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 
-import Link from "next/link";
-
-import { getPosts } from "@/lib/api";
-
 import BlogCard from "@/components/blogcard";
 
 export default async function Page() {
-  const posts = getPosts();
 
   return (
-
       <div className="max-w-2xl mx-auto mt-16 scroll-smooth ">
         <div className="mt-6 flex flex-col space-y-8">
           <BlogCard />
