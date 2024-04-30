07c6cef87261a85b7e17fb7bf3c6800b46fb32f8
Nuwan Perera <nuwanperera613@gmail.com>
2024-04-30 19:40:15 +0530

    feat: add page transition

---
+++ app/template.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function Templete({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+        x: 0,
+        y: -20,
+      }}
+      animate={{
+        opacity: 1,
+        x: 0,
+        y: 0,
+      }}
+      exit={{ opacity: 0, x: 0, y: 0 }}
+      transition={{
+        duration: 0.5,
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
